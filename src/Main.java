85d643465c09d9f1d4d17c2ea66e7676d2f66dab
Aleksei99 <smurahaleha@gmail.com>
2020-03-24 20:31:15 +0300

    edited

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("new messavms;vmger was created");
+        System.out.println("new message was created");
+
     }
 }
