b8e8291538b6bc2ed4fb1611c012b5826b7fe9ac
Makar-tester <makariv2004@gmail.com>
2020-03-24 20:27:34 +0300

    messanger borrow

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("new messager was created");
+        System.out.println("new messavms;vmger was created");
     }
 }
