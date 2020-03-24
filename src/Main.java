aca957264f89e4fb4e0286f80e1b5bc24eca6195
Aleksei99 <smurahaleha@gmail.com>
2020-03-24 20:44:37 +0300

    edited

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("new message was never created");
+        System.out.println("new message was never ev created");
 
     }
 }
