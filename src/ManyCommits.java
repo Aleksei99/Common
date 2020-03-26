cb5e64e8f54fd4415812ef2038e2e818f849b11f
Makar-tester <makariv2004@gmail.com>
2020-03-26 20:39:39 +0300

    many commits second one

--- src/ManyCommits.java
+++ src/ManyCommits.java
@@ -3,6 +3,7 @@ public class ManyCommits {
     public static void main(String[] args) {
         System.out.println("first one");
         System.out.println("second one");
+        System.out.println("third one");
     }
 
 }
